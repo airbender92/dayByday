38102fc8216f5b2bee411ccb66de09229f0eb0bf
wangyunbo <yambo1992@outlook.com>
2021-11-17 18:13:44 +0800

    npm

---
+++ npm/切换仓库.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: wangyunbo
+ * @Date: 2021-11-17 18:13:19
+ * @LastEditors: wangyunbo
+ * @LastEditTime: 2021-11-17 18:13:20
+ * @FilePath: \dayByday\npm\切换仓库.js
+ * @Description: file content
+ */
+npm config set registry http://128.196.0.125:9000/repository/npm-taobao/
