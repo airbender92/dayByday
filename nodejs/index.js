5c2b55b27e878f41e5c2a47e61a64211716e2048
wangyunbo <yambo1992@outlook.com>
2021-12-17 17:51:07 +0800

    npx

--- nodejs/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: wangyunbo
- * @Date: 2021-05-24 08:53:19
- * @LastEditors: wangyunbo
- * @LastEditTime: 2021-05-24 08:53:20
- * @Description: file content
- * @FilePath: \dayByday\nodejs\index.js
- */
